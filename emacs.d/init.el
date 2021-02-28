ebb6d44df981b4a383dcc310f9f823fc53f8bf9f
Guowei Lv <lvguowei1002@gmail.com>
2021-02-28 20:25:45 +0200

    update

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -7,29 +7,26 @@
 
 ;; Refresh
 (unless package-archive-contents
- (package-refresh-contents))
+  (package-refresh-contents))
 
-;; Bootstrap `use-package'
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+;; Bootstrap Quelpa
+(unless (package-installed-p 'quelpa)
+  (with-temp-buffer
+    (url-insert-file-contents "https://raw.githubusercontent.com/quelpa/quelpa/master/quelpa.el")
+    (eval-buffer)
+    (quelpa-self-upgrade)))
 
-(require 'use-package)
+(quelpa
+ '(quelpa-use-package
+   :fetcher git
+   :url "https://github.com/quelpa/quelpa-use-package.git"))
+
+(setq use-package-ensure-function 'quelpa)
+
+(require 'quelpa-use-package)
+
 (setq use-package-always-ensure t)
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/emacs_config.org"))
 
 ;; Custom stuff starting from here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (doom-modeline undo-tree use-package magit))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(mode-line ((t (:height 0.85))))
- '(mode-line-inactive ((t (:height 0.85)))))
